0a3f84610d9f7ebf9574f021badc5555456beb52
carlitos <carloserocha27@gmail.com>
2022-04-10 22:01:22 -0300

    wip: update order

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { User } from '../user/entities/user.entity';
@@ -14,7 +14,7 @@ export class OrderService {
     @InjectRepository(Order) private readonly orderRepository: Repository<Order>,
     @InjectRepository(Items) private readonly itemsRepository: Repository<Items>,
     @InjectRepository(Product) private readonly productRepository: Repository<Product>
-  ) {}
+  ) { }
 
   async create(user: User, createOrderDto: CreateOrderDto) {
     const products = [];
@@ -22,19 +22,19 @@ export class OrderService {
     let totalValue = 0;
     for (const item of createOrderDto.items) {
       const product = await this.productRepository.findOne({ id: item.id });
-      if (product) {
-        const price = product.price / 100;
 
-        totalValue += (price * item.quantity);
-
-        const createOrderItem = this.itemsRepository.create({
-          ...product,
-          quantity: item.quantity
-        })
-        const orderItem = await this.itemsRepository.save(createOrderItem);
-
-        products.push(orderItem);
+      if (!product) {
+        throw new NotFoundException(`Product ${item.id} not found`);
       }
+
+      const price = product.price / 100;
+
+      totalValue += price * item.quantity;
+
+      const createOrderItem = this.itemsRepository.create({ ...product, quantity: item.quantity });
+      const orderItem = await this.itemsRepository.save(createOrderItem);
+
+      products.push(orderItem);
     }
 
     const order = this.orderRepository.create({
@@ -46,25 +46,67 @@ export class OrderService {
     return this.orderRepository.save(order);
   }
 
-  findAll(userId: string) {
-    return this.orderRepository.find({
+  async findAll(userId: string) {
+    const orders = await this.orderRepository.find({
       relations: ['items'],
       where: {
         user: { id: userId }
       },
     });
+
+    if (!orders.length) throw new NotFoundException();
+
+    return orders.map(order => order.totalValue = order.totalValue / 100);
   }
 
-  findOne(user: User, id: string) {
-    return this.orderRepository.findOne({
+  async findOne(user: User, id: string) {
+    const order = await this.orderRepository.findOne({
+      relations: ['items'],
       where: {
         id,
         user,
       },
     });
+
+    if (!order) throw new NotFoundException();
+
+    order.totalValue = order.totalValue / 100
+
+    return order;
   }
 
-  update(id: string, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
+  async update(user: User, id: string, updateOrderDto: UpdateOrderDto) {
+    const orderWhere = { where: { id, user } };
+    const order = await this.orderRepository.findOne(orderWhere);
+
+    if (!order) throw new NotFoundException({ message: 'Order not found' });
+
+    const itemWhere = { where: { order: { id } } };
+    const items = await this.itemsRepository.find(itemWhere);
+
+    for (const item of updateOrderDto.items) {
+      if (item.quantity <= 0) {
+        throw new BadRequestException({ message: 'Quantity must be greater than 0' });
+      }
+
+      const saved = items.find(i => i.id === item.id);
+      if (!saved) throw new NotFoundException({ message: `Item ${item.id} not found` });
+
+      saved.quantity -= item.quantity || -1;
+
+      if (saved.quantity < 0) {
+        throw new BadRequestException({ message: `Item ${item.id} already received` });
+      }
+
+      const createOrderItem = this.itemsRepository.create(saved);
+      
+      await this.itemsRepository.save(createOrderItem);
+    }
+
+    const updatedOrder = await this.orderRepository.findOne({ ...orderWhere, relations: ['items'] });
+
+    updatedOrder.totalValue = updatedOrder.totalValue / 100;
+
+    return updatedOrder;
   }
 }

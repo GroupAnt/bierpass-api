76838adf862b68b63476ce30389c52ce221aeb87
carloserocha <carloserocha27@gmail.com>
2022-05-06 00:07:20 -0300

    simple log

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Logger } from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { CreateNotificationMercadoPagoDto } from './dto/create-notification.dto';
 
 @Controller('notification')
 export class NotificationController {
+  private readonly logger = new Logger(NotificationService.name)
+
   constructor(private readonly notificationService: NotificationService) {}
 
   @Post('/payment/mercadopago')
-  createPayment(@Body() createNotificationDto: CreateNotificationMercadoPagoDto) {
-    return this.notificationService.createMercadoPago(createNotificationDto);
+  async createPayment(@Body() createNotificationDto: CreateNotificationMercadoPagoDto) {
+    try {
+      return await this.notificationService.createMercadoPago(createNotificationDto);
+    } catch(error) {
+      this.logger.warn(`Payment Notification Error - ${error}`, createNotificationDto);
+
+      throw error;
+    }
   }
 }
